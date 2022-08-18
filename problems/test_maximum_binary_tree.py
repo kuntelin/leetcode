9c026a7a3d57500e0093ab7b4e8ea7af7654eeb7
Kunte Lin <kuntelin@gmail.com>
2022-08-18 15:22:17 +0800

    Add maximum binary tree

---
+++ problems/test_maximum_binary_tree.py
@@ -0,0 +1,84 @@
+import logging
+from typing import (
+    Optional,
+    List,
+)
+
+import pytest
+
+from problems.utils import (
+    TreeNode,
+    tree_node_to_list,
+)
+
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        if not 1 <= len(nums) <= 1000:
+            raise ValueError
+
+        if any(map(lambda x: not 0 <= x <= 1000, nums)):
+            raise ValueError
+
+        if len(nums) != len(set(nums)):
+            raise ValueError
+
+        def _find_max_index(items):
+            # max_value, max_index = -1, -1
+            # for index, item in enumerate(items):
+            #     if item > max_value:
+            #         max_value = item
+            #         max_index = index
+            #
+            # return max_index
+            return items.index(max(items))
+
+        def _construct_children(node_values) -> Optional[TreeNode]:
+            node_length = len(node_values)
+
+            if node_length == 0:
+                return None
+
+            if node_length == 1:
+                return TreeNode(node_values[0])
+
+            max_index = _find_max_index(node_values)
+
+            node_object = TreeNode(node_values[max_index])
+
+            left_values = node_values[:max_index]
+            if left_values:
+                node_object.left = _construct_children(left_values)
+
+            right_values = node_values[max_index + 1:]
+            if right_values:
+                node_object.right = _construct_children(right_values)
+
+            return node_object
+
+        return _construct_children(nums)
+
+
+class TestSolution:
+    def setup_class(self):
+        self.solution = Solution()
+
+    def teardown_class(self):
+        pass
+
+    def setup_method(self, method):
+        pass
+
+    @pytest.mark.parametrize(
+        ["nums", "result"],
+        [
+            ([3, 2, 1, 6, 0, 5], [6, 3, 5, None, 2, 0, None, None, 1]),
+        ],
+    )
+    @pytest.mark.benchmark
+    def test_constructMaximumBinaryTree(self, nums, result):  # noqa
+        logging.debug(f'{nums=}, {result=}')
+        assert tree_node_to_list(self.solution.constructMaximumBinaryTree(nums)) == result
+
+    def teardown_method(self, method):
+        pass
